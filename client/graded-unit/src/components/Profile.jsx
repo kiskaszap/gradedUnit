bee72ce96ab513080d65ff73cce2e4cbfeb42016
Balint Kaszap-Nagy <kaszap.nagy.balint@gmail.com>
2023-04-30 15:41:56 +0100

    server setup

--- client/graded-unit/src/components/Profile.jsx
+++ client/graded-unit/src/components/Profile.jsx
@@ -5,7 +5,7 @@ import AdminProfileForm from './AdminProfileForm'
 
 
 const Profile = () => {
-const [isAdmin,setIsAdmin]=useState(true)
+const [isAdmin,setIsAdmin]=useState(false)
   return (
     <div className='my-6 w-full px-auto  flex flex-col justify-center items-center'>
      <ImageUploader />
