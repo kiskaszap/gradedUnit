3080938cc8047df3249e58b16d399d4ffeb986a9
Balint Kaszap-Nagy <kaszap.nagy.balint@gmail.com>
2023-05-03 16:55:56 +0100

    setup mongodb

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const db = require('./mongodb.js');
 app.get('/', (req, res) => {
-  res.send('Hello, wld!');
+  res.send('Hello, world!');
 });
 
 app.listen(5000, () => {
