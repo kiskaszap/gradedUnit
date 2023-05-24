3bab4dc4f8e6cd92ecd91dc692dc93296f0bca09
Balint Kaszap-Nagy <kaszap.nagy.balint@gmail.com>
2023-05-24 18:30:20 +0100

    finishing gallery fetch

--- client/graded-unit/src/components/GalleryCarousel.jsx
+++ client/graded-unit/src/components/GalleryCarousel.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
 
 //assets
-import Gallery1 from '../assets/gallery1.jpg'
-import Gallery2 from '../assets/gallery2.jpg'
-import Gallery3 from '../assets/gallery3.jpg'
-import Gallery4 from '../assets/gallery4.jpg'
-import Gallery5 from '../assets/gallery5.jpg'
-import Gallery6 from '../assets/gallery6.jpg'
 
 const GalleryCarousel = () => {
   return (
@@ -14,56 +8,7 @@ const GalleryCarousel = () => {
 
   
 <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
-  <div class="-m-1 flex flex-wrap md:-m-2">
-    <div class="flex w-1/3  flex-wrap">
-      <div class="w-full p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src={Gallery1} />
-      </div>
-    </div>
-    <div class="flex w-1/3 flex-wrap">
-      <div class="w-full p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src={Gallery2} />
-      </div>
-    </div>
-    <div class="flex w-1/3 flex-wrap">
-      <div class="w-full p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src={Gallery3} />
-      </div>
-    </div>
-    <div class="flex w-1/3 flex-wrap">
-      <div class="w-full p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src={Gallery4} />
-      </div>
-    </div>
-    <div class="flex w-1/3 flex-wrap">
-      <div class="w-full p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src={Gallery5} />
-      </div>
-    </div>
-    <div class="flex w-1/3 flex-wrap">
-      <div class="w-full p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src={Gallery6} />
-      </div>
-    </div>
-  </div>
+  
 </div>
 
 </>
